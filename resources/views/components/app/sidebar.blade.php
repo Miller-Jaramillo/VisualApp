a74046e643e1e0a1ff3adc1754e415f11638ac3d
Miller Jaramillo <millerjaramillo.mj@gmail.com>
2024-04-22 16:35:09 -0500

    Se ajusto el label atenticacion

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -230,7 +230,7 @@
                                         <path class="fill-current text-slate-400" d="M15 12L8 6v5H0v2h8v5z" />
                                     </svg>
                                     <span
-                                        class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Autenticaion</span>
+                                        class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Autenticacion</span>
                                 </div>
                                 <!-- Icon -->
                                 <div
